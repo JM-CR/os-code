43309be0176349d4dd6abbb3d2e0e7c57d47147d
Josue Mosh <msi_98_98@yahoo.com.mx>
2020-02-19 09:33:56 -0600

    General template to create any C library

---
+++ Template/library.c
@@ -0,0 +1,31 @@
+// See library.h for more info
+// Author: Josue Mosiah Contreras Rocha
+// File: library.c
+// Date: 17/02/20
+
+// ------------------------------------------
+// System and aplication specific headers
+// ------------------------------------------
+#include "library.h"
+
+
+// -----------------------------
+// Private elements
+// -----------------------------
+
+// Private global variables
+
+/* Private macros and constants */
+
+/* Private types */
+
+/* Private global variables */
+
+/* Private functions */
+
+
+// -----------------------------
+// Public elements
+// -----------------------------
+
+/* Implementation of the public functions */
---
+++ Template/library.h
@@ -0,0 +1,25 @@
+// Template to create C libraries.
+// Author: Josue Mosiah Contreras Rocha
+// File: library.h
+// Date: 17/02/20
+
+// -----------------------------
+// System headers
+// -----------------------------
+
+
+// -----------------------------
+// Public interface
+// -----------------------------
+
+/* Constants */
+
+/* Types declarations */
+
+/* Global variables */
+
+/* Function prototypes */
+
+/**
+ * Function's documentation.
+ */
