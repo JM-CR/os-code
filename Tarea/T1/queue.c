b1c49457026bc0d866602b6d882c04302c900837
Josue Mosh <msi_98_98@yahoo.com.mx>
2020-02-19 10:27:04 -0600

    Queue library for burst nodes

---
+++ Tarea/T1/queue.c
@@ -0,0 +1,72 @@
+// See queue.h for more info
+// Author: Josue Mosiah Contreras Rocha
+// File: queue.c
+// Date: 17/02/20
+
+// ------------------------------------------
+// System and aplication specific headers
+// ------------------------------------------
+#include <stdlib.h>
+#include <stdio.h>
+#include "queue.h"
+
+
+// -----------------------------
+// Public elements
+// -----------------------------
+
+/* Implementation of the public functions */
+
+Node_t *next( Node_t *item ) {
+    return item->next;
+}
+
+void insert( Node_t **ppRoot, Node_t *item ) {
+    // First item
+    Node_t *pRoot = *ppRoot;
+    if ( pRoot == NULL ) {
+        *ppRoot = item;
+        return;
+    }
+
+    // Traverse queue
+    Node_t *currentNode = pRoot;
+    while ( hasNext(currentNode) ) {
+        currentNode = currentNode->next;
+    }
+
+    // Insert element
+    currentNode->next = item;
+}
+
+void printQueue( Node_t *pRoot ) {
+    // Initialize
+    printf("Elements: ");
+    Node_t *node = pRoot;
+
+    // Traverse
+    while ( node != NULL ) {
+        printf("Lifetime: %d ", node->element);
+        node = node->next;
+    }
+}
+
+bool hasNext( Node_t *item ) {
+    return item->next == NULL ? false : true;
+}
+
+void erase( Node_t **ppRoot ) {
+    // Validation
+    Node_t *pRoot = *ppRoot;
+    if ( pRoot == NULL )
+        return;
+
+    // Check next
+    if ( !hasNext(pRoot) ) {
+        free(pRoot);
+        *ppRoot = NULL;
+    } else {
+        *ppRoot = (*ppRoot)->next;
+        free(pRoot);
+    }
+}
---
+++ Tarea/T1/queue.h
@@ -0,0 +1,55 @@
+// Specific functions to create and handle a queue.
+// Author: Josue Mosiah Contreras Rocha
+// File: queue.h
+// Date: 17/02/20
+
+// -----------------------------
+// System headers
+// -----------------------------
+#include <stdbool.h>
+#include "burst.h"
+
+
+// -----------------------------
+// Public interface
+// -----------------------------
+
+/* Function prototypes */
+
+/**
+ * Gets the next node.
+ *
+ * @param item Element to traverse.
+ * @return Pointer to the next element.
+ */
+Node_t *next( Node_t *item );
+
+/**
+ * Inserts a new node in the queue.
+ *
+ * @param ppRoot Pointer to the first node.
+ * @param item Node to insert.
+ */
+void insert( Node_t **ppRoot, Node_t *item );
+
+/**
+ * Prints the whole queue.
+ *
+ * @param pRoot Pointer to the first node.
+ */
+void printQueue( Node_t *pRoot );
+
+/**
+ * Checks if there is a node after the item.
+ *
+ * @param item Node to check.
+ * @return True if there is the next element exists.
+ */
+bool hasNext( Node_t *item );
+
+/**
+ * Deletes the first node.
+ * 
+ * @param ppRoot Pointer to the first element
+ */
+void erase( Node_t **ppRoot );
