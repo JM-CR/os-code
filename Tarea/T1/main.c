b1ecc1880c079b7e4a2097af60521b3a4220cc8d
bvideal <brunovalfe@gmail.com>
2020-02-23 18:42:18 -0600

    listas incompletas

--- Tarea/T1/main.c
+++ Tarea/T1/main.c
@@ -9,7 +9,7 @@
 #include <stdbool.h>
 #include "queue.h"
 
-#define TOTAL 20
+#define TOTAL 5
 
 int main(void) {
     // Create root nodes
@@ -27,6 +27,78 @@ int main(void) {
 
     for( int i = 0; i < TOTAL; i++ )
         printQueue(process[i]);
+
+/*
+FIFO(process, TOTAL);
+ FIFO{
+     Node_t *node;
+     int end, iterator;
+     bool first = TRUE;
+     do{
+         node = process[iterator];
+         if(node != NULL){//existing proc
+            if(first){
+                exectime += node->lifeTime;
+                node->lifetime = 0;
+                first = FALSE;
+                erase(node);
+            }
+            if(node == NULL){//end of proc
+                end++;
+                erase(node);
+            }
+            else{// type 'e'
+                do{
+                    node->lifetime--;
+                }while(node->lifeTime != 0);
+            }
+            do{//type 'c'
+                
+            }while(node->lifeTime != 0);
+            node = node->next;
+            
+            if(iterator > TOTAL){
+                iterator = 0;
+            }
+        }
+        iterator++;
+     }while(end!=TOTAL);
+ */
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
